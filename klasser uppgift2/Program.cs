26f105f90388e996641b55a8e74c334b0b00d7fa
7R1XY <theo.broden@gmail.com>
2024-04-03 10:48:43 +0200

    Add project files.

---
+++ klasser uppgift2/Program.cs
@@ -0,0 +1,15 @@
+﻿using klasser_uppgift2;
+using System;
+namespace Uppgift2
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            namn p = new namn("Ameen", "Hashram");
+
+            Console.WriteLine(p.firstname + " " + p.lastname);
+            
+        }
+    }
+}
---
+++ klasser uppgift2/namn.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace klasser_uppgift2
+{
+    class namn
+    {
+        public string? firstname;
+        public string? lastname;
+
+        public namn(string fn, string ln)
+        {
+            firstname = fn;
+            lastname = ln;
+        }
+    }
+}
